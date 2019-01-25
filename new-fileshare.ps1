054e14d116c586924c8c3b6fe6c11f671b60feeb
filipremahl <46345405+filipremahl@users.noreply.github.com>
2019-01-25 13:29:30 +0100

    Update new-fileshare.ps1

--- new-fileshare.ps1
+++ new-fileshare.ps1
@@ -1,5 +1,7 @@
 New-Item -Path C:\Users\qshare\resurser\ -ItemType Directory
 New-SmbShare -Name resurser -Path C:\Users\qshare\resurser
+Grant-SmbShareAccess -Name resurser -AccountName "Alla" -AccessRight full
 
 New-Item -Path C:\Users\qshare\gemensam\ -ItemType Directory
 New-SmbShare -Name gemensam -Path C:\Users\qshare\gemensam\
+Grant-SmbShareAccess -Name gemensam -AccountName "Alla" -AccessRight full
