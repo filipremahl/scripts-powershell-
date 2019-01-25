b862c82508a5fa9510ecc228a723d6df9afbc42d
filipremahl <46345405+filipremahl@users.noreply.github.com>
2019-01-25 13:25:41 +0100

    Update and rename create-privateshare to create-privateshare.ps1

--- create-privateshare.ps1
+++ create-privateshare.ps1
@@ -6,4 +6,6 @@ $acc = $i.SamAccountName
 New-Item -Path C:\Users\qshare\$user\ -ItemType Directory
 New-SmbShare -Name qshare -Path C:\Users\qshare\$user\
 Grant-SmbShareAccess -Name $user -AccountName $acc -AccessRight full 
+$cim = New-CimSession -ComputerName "testest123"
+Revoke-SmbShareAccess -Name qshare -AccountName "Alla" -Force -CimSession $cim
 }
